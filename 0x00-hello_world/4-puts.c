bef088c1fe8318c3afaf1d52686a889248d11a87
ayoubf <foudalayoub1998@gmail.com>
2023-03-16 14:52:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 
 /**
- * this is comment
- *voila
- *la
- *description
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
