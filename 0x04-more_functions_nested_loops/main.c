0d2f4f87343f8637ad37d47c9f46a7e39532d965
ayoubf <foudalayoub1998@gmail.com>
2023-03-24 18:00:46 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,59 @@
+#ifndef _ISUPPER_H_
+#define _ISUPPER_H_
+int _isupper(int c);
+#endif
+
+#ifndef _ISDIGIT_H_
+#define _ISDIGIT_H_
+int _isdigit(int c);
+#endif
+
+#ifndef _MUL_H_
+#define _MUL_H_
+int mul(int a, int b);
+#endif
+
+#ifndef _PRINT_NUMBERS_H_
+#define _PRINT_NUMBERS_H_
+void print_numbers(void);
+#endif
+
+#ifndef _PUTCHAR_H_
+#define _PUTCHAR_H_
+int _putchar(char c);
+#endif
+
+#ifndef _PRINT_MOST_NUMBERS_H_
+#define _PRINT_MOST_NUMBERS_H_
+void print_most_numbers(void);
+#endif
+
+#ifndef _MORE_NUMBERS_H_
+#define _MORE_NUMBERS_H_
+void more_numbers(void);
+#endif
+
+#ifndef _PRINT_LINE_H_
+#define _PRINT_LINE_H_
+void print_line(int n);
+#endif
+
+#ifndef _PRINT_DIAGONAL_H_
+#define _PRINT_DIAGONAL_H_
+void print_diagonal(int n);
+#endif
+
+#ifndef _PRINT_SQUARE_H_
+#define _PRINT_SQUARE_H_
+void print_square(int size);
+#endif
+
+#ifndef _PRINT_TRIANGLE_H_
+#define _PRINT_TRIANGLE_H_
+void print_triangle(int size);
+#endif
+
+#ifndef _PRINT_NUMBER_H_
+#define _PRINT_NUMBER_H_
+void print_number(int n);
+#endif
